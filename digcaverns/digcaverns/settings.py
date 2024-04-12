d0e890c408e2bd5a74cbce63a1bbedf5ef75ff81
Alberto <albertovalleflores99@gmail.com>
2024-04-12 16:57:34 -0700

    Add static files to vercel

--- digcaverns/digcaverns/settings.py
+++ digcaverns/digcaverns/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uml^4arjkxg5pv@v2=!^*q)68x56sx=701@8d7(zs!hsn3irqk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
