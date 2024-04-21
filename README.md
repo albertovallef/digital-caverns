7ad28344be0c354fc45962606c3fca1fae1ff572
Alberto <albertovalleflores99@gmail.com>
2024-04-21 01:00:46 -0700

    Add dockerfiles and db setup

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # digital-caverns
 This is my personal website and blog
+
+# Requirements
+- Python 3.9
+    - Install `requirements.txt`
+- Docker
+    - docker compose plugin
+
+# Run (with venv)
+```bash
+python manage.py runserver
+```
+
+# Run (with Docker)
+ ```bash
+ sudo docker compose up
+ ```
+
+ ```bash
+ sudo docker compose down -v 
+ ```
+
+
--- digcaverns/digcaverns/settings.py
+++ digcaverns/digcaverns/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = 'digcaverns.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    "ENGINE": "django.db.backends.postgresql",
-    #    "NAME": os.environ.get("POSTGRES_DATABASE"),
-    #    "USER": os.environ.get('POSTGRES_USER'),
-    #    "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
-    #    "HOST": os.environ.get("POSTGRES_HOST"),
-    #    "URL": os.environ.get("POSTGRES_URL"),
-    #    "PORT": "5432"
-    #    # 'ENGINE': 'django.db.backends.sqlite3',
-    #    # 'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DATABASE"),
+        "USER": os.environ.get('POSTGRES_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "URL": os.environ.get("POSTGRES_URL"),
+        "PORT": "5432"
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
