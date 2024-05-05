dcbca5f15e3a2be7301a71cfe13739991132c83a
Alberto <albertovalleflores99@gmail.com>
2024-05-05 15:25:46 -0700

    Refactor: change resume attachment name

--- digcaverns/mysite/views.py
+++ digcaverns/mysite/views.py
@@ -17,7 +17,8 @@ def resume(request):
 @xframe_options_exempt
 def display_resume(request):
     """Returns requested pdf resource"""
-    # Change this to the path where your PDF files are stored
+    # TODO: Implement a system where I grab the most 
+    # recent resume automatically; relates to issue#4 
     filename = "Resume-15-08-2022.pdf"
     file_path = os.path.join(settings.MEDIA_ROOT, filename)
     print(file_path)
@@ -25,7 +26,7 @@ def display_resume(request):
     if os.path.exists(file_path):
         with open(file_path, 'rb') as pdf:
             response = HttpResponse(pdf.read(), content_type='application/pdf')
-            response['Content-Disposition'] = 'inline; filename="' + os.path.basename(file_path) + '"'
+            response['Content-Disposition'] = 'inline; filename="Alberto_Valle_Resume.pdf"'
             return response
     else:
         return HttpResponse("File not found.", status=404)
