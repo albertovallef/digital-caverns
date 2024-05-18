3af3bac1125c94eb64c2a730831871b085b1fa58
Alberto <albertovalleflores99@gmail.com>
2024-05-18 15:09:45 -0700

    Fix: incorrect flag on debug

--- digcaverns/digcaverns/settings.py
+++ digcaverns/digcaverns/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('DEBUG') == str(1) else False
+DEBUG = True if os.environ.get('DEBUG') == str(0) else False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', 'www.digitalcaverns.dev']
 
